1b0707cf9a4609ca63cfd21ddfddde956ffbec7a
L RMN <lrmnid@gmail.com>
2023-05-30 16:22:27 +0700

    main

---
+++ src/script.js
@@ -0,0 +1,6 @@
+let progressBar = document.querySelector("#loading-bar");
+let profile = document.querySelector("#profile");
+window.onload = function() {
+  progressBar.style.display = 'none';
+  profile.style.filter = 'none';
+};
